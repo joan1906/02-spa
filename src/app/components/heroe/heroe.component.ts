2887ec9832c429159bfe5b4e354e4a82ff7a9c56
Joan Blanco <joanblanco1906@gmail.com>
2022-07-11 22:18:43 -0400

    creando el detalle del heroe

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../services/heroes.service';
+
 
 @Component({
   selector: 'app-heroe',
@@ -7,7 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+  heroe: any = {};
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private _heroesService: HeroesService)
+  {
+    this.activatedRoute.params.subscribe(params => {
+      this.heroe = this._heroesService.getHeroe(params['id']);
+    });
+  }
 
   ngOnInit(): void {
   }
