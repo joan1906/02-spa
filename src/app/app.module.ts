af441dfd3c4d81afb6bf790b130982cec526df4c
Joan Blanco <joanblanco1906@gmail.com>
2022-07-11 20:51:25 -0400

    eliminando archivos spect y creando component detalle heroe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     HomeComponent,
     AboutComponent,
     HeroesComponent,
+    HeroeComponent,
   ],
   imports: [BrowserModule, APP_ROUTING],
   providers: [HeroesService],
