334dbd1143bce7cac200afad4e9211eb63bcf09c
Joan Blanco <joanblanco1906@gmail.com>
2022-07-12 23:27:45 -0400

    agregando el filtrado de heroes

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../../services/heroes.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  heroes = [];
 
-  constructor() { }
+  constructor(private _heroesService: HeroesService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  buscarHeroe(termino: string) {
+    this.heroes = this._heroesService.buscarHeroes(termino);
   }
-
 }
